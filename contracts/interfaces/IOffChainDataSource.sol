17254ceeb3063dacb167af26e1fcd6ee0cef0426
metter <139591245+metter0@users.noreply.github.com>
2024-12-10 10:09:12 +0500

    Improved formatting Also added annotations in NatSpec format for each method

--- contracts/interfaces/IOffChainDataSource.sol
+++ contracts/interfaces/IOffChainDataSource.sol
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+/// @title IOffChainDataSource
+/// @notice Interface for interacting with an off-chain data source
 interface IOffChainDataSource {
+    /**
+     * @notice Retrieves data associated with a specific user
+     * @param user The address of the user whose data is being queried
+     * @return data The data associated with the given user
+     */
     function getData(address user) external view returns (string memory data);
+
+    /**
+     * @notice Initiates loading of data from an off-chain source
+     * @param user The address of the user for whom data is being loaded
+     * @param url The URL of the off-chain data source
+     * @param path The specific path within the data source to fetch
+     */
     function load(address user, string memory url, string memory path) external;
 }
